4c969f1d9f1e34afde7ed4ac2e0753c97c0339fd
powerseunghun <size411@naver.com>
2020-05-21 23:23:55 +0900

    add exception (vue modal) better UX

--- day6_practice/vue-todo/src/components/TodoInput.vue
+++ day6_practice/vue-todo/src/components/TodoInput.vue
@@ -6,14 +6,24 @@
       <i class="addBtn fas fa-plus" aria-hidden="true"></i>
     </span>
     <!--<h1>{{newTodoItem}}</h1>-->
+
+    <modal v-if="showModal" @close="showModal = false">
+      <h3 slot="header">경고</h3>
+      <span slot="footer" @click="showModal = false">
+        할 일을 입력하세요.
+        <i class="closeModalBtn fas fa-times" aria-hidden="true"></i>
+      </span>
+    </modal>
   </div>
 </template>
 
 <script>
+  import Modal from './common/Modal.vue'
   export default {
     data() {
       return {
-        newTodoItem: ''
+        newTodoItem: '',
+        showModal: false
       }
     },
     methods: {
@@ -23,10 +33,16 @@
           this.$emit('addTodo', value);
           this.clearInput();
         }
+        else {
+          this.showModal = !this.showModal;
+        }
       },
       clearInput() {
         this.newTodoItem = '';
       }
+    },
+    components: {
+      Modal: Modal
     }
   }
 </script>
