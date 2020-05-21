9de8c05c330d306d6497eb6307225a619c15b974
powerseunghun <size411@naver.com>
2020-05-21 23:11:46 +0900

    add vue animation

--- day6_practice/vue-todo/src/components/TodoList.vue
+++ day6_practice/vue-todo/src/components/TodoList.vue
@@ -1,14 +1,14 @@
 <template>
   <section>
-    <ul>
-      <li v-for="(todoItem, index) in propsdata" class="shadow">
+    <transition-group name="list" tag="ul">
+      <li v-for="(todoItem, index) in propsdata" :key="todoItem" class="shadow">
         <i class="checkBtn fas fa-check" aria-hidden="true"></i>
           {{ todoItem }}
           <span class="removeBtn" type="button" @click="removeTodo(todoItem, index)">
             <i class="far fa-trash-alt" aria-hidden="true"></i>
           </span>
       </li>
-    </ul>
+    </transition-group>
   </section>
 </template>
 
@@ -27,6 +27,13 @@
 </script>
 
 <style scoped>
+  .list-enter-active, .list-leave-active {
+    transition: all 1s;
+  }
+  .list-enter, .list-leave-to {
+    opacity: 0;
+    transform: translateY(30px);
+  }
   ul {
     list-style-type: none;
     padding-left: 0px;
